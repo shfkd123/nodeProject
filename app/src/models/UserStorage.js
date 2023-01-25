6b49ea77b04be62f3f360102b085e49275589879
shfkd123 <shfkd123@gmail.com>
2023-01-25 18:16:00 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -29,6 +29,16 @@ class UserStorage {
 
         return userInfo;
     }
+    
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        console.log(users);
+        return { success: true};
+        
+    }
 }
 
 module.exports = UserStorage;
