549397bd05b3ade4d79a319cfb6c92caa7d869d2
shfkd123 <shfkd123@gmail.com>
2023-01-25 17:55:22 +0900

    회원가입 요청 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,21 +2,15 @@
 
 const id = document.querySelector("#id"),
 psword = document.querySelector("#psword"),
-    loginbtn = document.querySelector("button");
-
-//console.log(id);
+    loginbtn = document.querySelector("#button");
 
 loginbtn.addEventListener("click", login);
 
 function login() {
-    //console.log(id.value);
     const req = {
         id: id.value,
         psword: psword.value,
     };
-    //console.log(req);
-    //console.log(JSON.stringify(req)); //문자열
-    
 
     fetch("/login",{
         method: "POST",
